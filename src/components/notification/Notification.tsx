c554c64b039c651e6967e27d23618921f71ed230
imykhailychenko <igor.c.m@ukr.net>
2020-04-13 17:23:59 +0300

    done

--- src/components/notification/Notification.tsx
+++ src/components/notification/Notification.tsx
@@ -10,7 +10,7 @@ const Notification: React.FC<Props> = ({ alert, onWarnning }) => (
   <div className={styles.wrp}>
     <div className="popup">
       <h3 className={styles.title}>Attention!</h3>
-      <p className={styles.text}>This number is alredy bilong to another contact</p>
+      <p className={styles.text}>This number is already belong to another contact</p>
       <button className={styles.btn} onClick={() => onWarnning(alert)} type="button">
         Ok
       </button>
